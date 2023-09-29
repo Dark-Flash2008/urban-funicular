ec73277978371960c8b813b8998a82452f4963d1
Dark Flash <rishikumarbose1@gmail.com>
2023-09-29 22:00:38 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Some might be getting diffcult about new syntax style present in JDK-21 LTS. So, for them is this is solution. Right now I'm using BlueJ IDE, it can be use on any IDE.
+ */
+
+class HelloWorld
+{
+    static void main()
+    {
+        System.out.println("Hello, World!");
+        }
+}
